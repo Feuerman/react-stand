6bf520964c651fe37de221f4a25824df89f99941
Feuerman <valerymarkunin@gmail.comgit config --global user.email valerymarkunin@gmail.com>
2018-01-02 11:28:16 +0300

    start app

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,38 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: ["./src/js/app.js"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "js/[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: "./dist"
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
